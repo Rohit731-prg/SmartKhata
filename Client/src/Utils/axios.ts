4eb5faca7a19a62da0774de82d1b9844cda6d54b
Rohit731-prg <rohitsingha821380@gmail.com>
2026-09-18 01:25:40 +0530

    fix production api url

--- Client/src/Utils/axios.ts
+++ Client/src/Utils/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL:
+        import.meta.env.VITE_API_URL ||
+        "https://smartkhata-ve7h.onrender.com/api",
     withCredentials: true,
 });
